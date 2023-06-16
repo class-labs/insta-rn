6999a2f2e11eb38ea653bbb0b09e0b8656e1bf26
Simon <sstur@me.com>
2023-06-16 12:46:46 -0700

    Add pull-to-refresh

--- app/index.tsx
+++ app/index.tsx
@@ -5,14 +5,22 @@ import { getPosts } from "../src/api/getPosts";
 import { HomeFeed } from "../src/components/HomeFeed";
 
 export default () => {
-  const { data, error } = useQuery(["posts"], getPosts);
+  const { data, error, isLoading, refetch } = useQuery(["posts"], getPosts);
   if (error) {
     return <Paragraph>{String(error)}</Paragraph>;
   }
   return (
     <>
       <Stack.Screen options={{ title: "Home" }} />
-      {!data ? <Paragraph>Loading...</Paragraph> : <HomeFeed posts={data} />}
+      {!data ? (
+        <Paragraph>Loading...</Paragraph>
+      ) : (
+        <HomeFeed
+          posts={data}
+          isRefreshing={isLoading}
+          onRefresh={() => refetch()}
+        />
+      )}
     </>
   );
 };
