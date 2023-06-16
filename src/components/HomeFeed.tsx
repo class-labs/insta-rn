7bac9c1a8303817d81ce134ee576d89bcf41778a
Simon <sstur@me.com>
2023-06-16 12:46:46 -0700

    Add pull-to-refresh

--- src/components/HomeFeed.tsx
+++ src/components/HomeFeed.tsx
@@ -1,14 +1,23 @@
 import { Paragraph, ScrollView } from "tamagui";
 import { FeedPost } from "../types/FeedPost";
+import { RefreshControl } from "react-native";
 
 type Props = {
   posts: Array<FeedPost>;
+  isRefreshing: boolean;
+  onRefresh: () => void;
 };
 
 export function HomeFeed(props: Props) {
-  const { posts } = props;
+  const { posts, isRefreshing, onRefresh } = props;
   return (
-    <ScrollView flex={1} backgroundColor="white">
+    <ScrollView
+      flex={1}
+      backgroundColor="white"
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
+      }
+    >
       {posts.map((post) => (
         <Paragraph>{post.caption}</Paragraph>
       ))}
