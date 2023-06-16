e186e1c0ef90fd1d3642e80f2fecf118043d11b6
Simon <sstur@me.com>
2023-06-16 11:32:07 -0700

    Add auth and async-storage

--- src/api/getPosts.ts
+++ src/api/getPosts.ts
@@ -1,8 +1,11 @@
 import { FeedPost } from "../types/FeedPost";
+import { getAuthHeaders } from "./getAuthHeaders";
 import { toUrl } from "./toUrl";
 
 export async function getPosts() {
-  const response = await fetch(toUrl("/posts"));
+  const response = await fetch(toUrl("/posts"), {
+    headers: getAuthHeaders(),
+  });
   const posts: Array<FeedPost> = await response.json();
   return posts;
 }
---
+++ src/support/Auth.tsx
@@ -0,0 +1,57 @@
+import { ReactNode, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const STORAGE_KEY = "@auth-token";
+
+let authToken: string | null = null;
+
+// A set of functions that will be invoked when the auth token changes
+const listeners = new Set<() => void>();
+
+export function getAuthToken() {
+  return authToken;
+}
+
+function setAuthToken(token: string | null) {
+  authToken = token;
+  if (token === null) {
+    AsyncStorage.removeItem(STORAGE_KEY);
+  } else {
+    AsyncStorage.setItem(STORAGE_KEY, token);
+  }
+  for (const listener of listeners) {
+    listener();
+  }
+}
+
+function addListener(listener: () => void) {
+  listeners.add(listener);
+  return () => {
+    listeners.delete(listener);
+  };
+}
+
+export function AuthProvider(props: { children: ReactNode }) {
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    AsyncStorage.getItem(STORAGE_KEY).then((token) => {
+      authToken = token;
+      setLoading(false);
+    });
+  }, []);
+  if (isLoading) {
+    return null;
+  }
+  return <>{props.children}</>;
+}
+
+export function useAuth() {
+  const [authToken, setLocalToken] = useState(() => getAuthToken());
+  useEffect(() => {
+    return addListener(() => {
+      const newToken = getAuthToken();
+      setLocalToken(newToken);
+    });
+  }, []);
+  return { authToken, setAuthToken };
+}
