e5da3321a0a007175ea7ca5fd1fb8953771b5dcd
Simon <sstur@me.com>
2023-06-17 10:58:33 -0700

    Setup for task2

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,17 @@
 import { Stack } from "expo-router";
-import { Paragraph, YStack } from "tamagui";
+import { Image, Paragraph, YStack } from "tamagui";
 
 import { posts } from "../src/posts";
 
 export default () => {
   return (
-    <YStack flex={1} padding={10}>
+    <YStack flex={1}>
       <Stack.Screen options={{ title: "Home" }} />
       {posts.map((post) => (
-        <Paragraph key={post.id}>{post.caption}</Paragraph>
+        <YStack key={post.id}>
+          <Image source={{ uri: post.photo }} aspectRatio={1} />
+          <Paragraph padding={10}>{post.caption}</Paragraph>
+        </YStack>
       ))}
     </YStack>
   );
