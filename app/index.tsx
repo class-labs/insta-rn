a46c7c82594425c24790a1a28c073e071710e3e7
Simon <sstur@me.com>
2023-06-17 10:56:00 -0700

    Task 1 solution

--- app/index.tsx
+++ app/index.tsx
@@ -7,12 +7,9 @@ export default () => {
   return (
     <YStack flex={1} padding={10}>
       <Stack.Screen options={{ title: "Home" }} />
-      {
-        // Task 1
-        // Render just the caption of each post in the `posts` array
-        // Hint: use array.map() to loop through the array
-        // Use a <Paragraph></Paragraph> to render it
-      }
+      {posts.map((post) => (
+        <Paragraph key={post.id}>{post.caption}</Paragraph>
+      ))}
     </YStack>
   );
 };
