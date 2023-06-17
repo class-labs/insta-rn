4f766217807197f6362bf0a3e28953e82b103e1d
Simon <sstur@me.com>
2023-06-16 22:49:58 -0700

    Add comments in post details

--- src/components/PostDetails.tsx
+++ src/components/PostDetails.tsx
@@ -1,5 +1,13 @@
 import { Alert, Pressable } from "react-native";
-import { Avatar, Image, Paragraph, ScrollView, XStack, YStack } from "tamagui";
+import {
+  Avatar,
+  Image,
+  Paragraph,
+  ScrollView,
+  View,
+  XStack,
+  YStack,
+} from "tamagui";
 import {
   Heart as IconHeart,
   MessageSquare as IconMessageSquare,
@@ -38,18 +46,9 @@ export function PostDetails() {
   if (!post) {
     return <Paragraph>Not Found</Paragraph>;
   }
-  const {
-    id,
-    author,
-    photo,
-    caption,
-    likedBy,
-    comments,
-    isLikedByViewer,
-    createdAt,
-  } = post;
+  const { id, author, photo, caption, likedBy, comments, isLikedByViewer } =
+    post;
   const likeCount = likedBy.length;
-  const commentCount = comments.length;
   return (
     <ScrollView flex={1} backgroundColor="white">
       <XStack px={16} py={12} alignItems="center" space={8}>
@@ -85,13 +84,35 @@ export function PostDetails() {
           {likeCount === 1 ? "1 like" : `${likeCount.toLocaleString()} likes`}
         </Paragraph>
         <Paragraph opacity={0.8}>{caption}</Paragraph>
-        <Paragraph fontWeight="600">
-          {commentCount === 1
-            ? "View 1 comment"
-            : `View ${commentCount.toLocaleString()} comments`}
-        </Paragraph>
-        <Paragraph opacity={0.4}>{formatRelativeTime(createdAt)}</Paragraph>
       </YStack>
+      <View height={1} backgroundColor="#e8e8e8" />
+      <YStack px={16} pt={16} pb={12} space={12}>
+        <Paragraph fontSize={14}>Comments ({comments.length})</Paragraph>
+        {comments.map((comment) => {
+          const { author } = comment;
+          return (
+            <XStack space={10}>
+              <Avatar circular size="$3">
+                <Avatar.Image source={{ uri: author.profilePhoto }} />
+                <Avatar.Fallback>
+                  <YStack flex={1} jc="center" ai="center">
+                    <Paragraph>{getInitials(author.name)}</Paragraph>
+                  </YStack>
+                </Avatar.Fallback>
+              </Avatar>
+              <YStack>
+                <Paragraph
+                  fontSize={14}
+                >{`${author.name}: ${comment.text}`}</Paragraph>
+                <Paragraph fontSize={12} opacity={0.4}>
+                  {formatRelativeTime(comment.createdAt)}
+                </Paragraph>
+              </YStack>
+            </XStack>
+          );
+        })}
+      </YStack>
+      <View height={1} backgroundColor="#e8e8e8" />
     </ScrollView>
   );
 }
