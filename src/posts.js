672c28a48fc709ce9f70eeca73bfb7f0c7398a48
Simon <sstur@me.com>
2023-06-17 10:21:01 -0700

    Setup for task 1

---
+++ src/posts.js
@@ -0,0 +1,239 @@
+export const posts = [
+  {
+    id: "zwb30aets",
+    author: {
+      id: "2gcwh1ccdfx",
+      name: "Kevin",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453596-ea862041-f1de-4e71-880b-2573c1f47ce8.jpg",
+      username: "kevin",
+    },
+    photo: "https://images.web-api.dev/images/c8qiots6ievz0igj8v7r2lz3.jpg",
+    caption: "This is a waterfall",
+    likeCount: 2,
+    commentCount: 2,
+    isLikedByViewer: true,
+    createdAt: "2023-06-16T22:25:47.596Z",
+  },
+  {
+    id: "1g19h2k4rbt",
+    author: {
+      id: "2gcwh1ccdfx",
+      name: "Kevin",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453596-ea862041-f1de-4e71-880b-2573c1f47ce8.jpg",
+      username: "kevin",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451334-1ee81ecb-cab0-47b9-a601-0fbfc0fd3fdd.jpeg",
+    caption: "Arizona cactus",
+    likeCount: 1,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-26T01:12:00.000Z",
+  },
+  {
+    id: "8anlbujp9b",
+    author: {
+      id: "vpk114e74l",
+      name: "Liza",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453597-cc7bff73-b838-4db6-a137-6706805195bf.jpg",
+      username: "liza",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451337-6b988d5f-af92-48cc-9c94-4e3f7a435f1c.jpeg",
+    caption: "No better time to be alive",
+    likeCount: 0,
+    commentCount: 1,
+    isLikedByViewer: false,
+    createdAt: "2022-09-26T01:12:00.000Z",
+  },
+  {
+    id: "u648mp5zw1",
+    author: {
+      id: "2gcwh1ccdfx",
+      name: "Kevin",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453596-ea862041-f1de-4e71-880b-2573c1f47ce8.jpg",
+      username: "kevin",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451339-f66ff3ae-eda6-4715-8896-7e021904a283.jpeg",
+    caption: "Stay frosty",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-26T01:12:00.000Z",
+  },
+  {
+    id: "2brxa2c2i5l",
+    author: {
+      id: "2fd9ok8eaaz",
+      name: "Zach",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453599-affcb8f9-b475-40c1-94dc-2f7d23f820c6.jpg",
+      username: "zach",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451342-6095f8d0-3138-4b5a-8e94-aede107f18a9.jpeg",
+    caption: "Enjoy life's subtle moments",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-26T01:12:00.000Z",
+  },
+  {
+    id: "qdonh4zwo7",
+    author: {
+      id: "2gcwh1ccdfx",
+      name: "Kevin",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453596-ea862041-f1de-4e71-880b-2573c1f47ce8.jpg",
+      username: "kevin",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451345-81ab917e-f1e2-4b61-8cf5-9d685dacab75.jpeg",
+    caption: "It's colder at night than outside",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-26T01:12:00.000Z",
+  },
+  {
+    id: "26m0r2hxo67",
+    author: {
+      id: "vpk114e74l",
+      name: "Liza",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453597-cc7bff73-b838-4db6-a137-6706805195bf.jpg",
+      username: "liza",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451346-acabcb5f-7f06-4113-bc54-adba78932a0b.jpeg",
+    caption: "Wyoming is great",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-26T01:12:00.000Z",
+  },
+  {
+    id: "1lvar1khrkl",
+    author: {
+      id: "29r39bb8kdr",
+      name: "Julie",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453593-560d6ae3-0e11-44dd-90f5-f0b87a8b4ce9.jpg",
+      username: "julie",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451348-dac99392-7620-4369-b653-4b894abc154e.jpeg",
+    caption: "Stay hydrated",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-26T01:12:00.000Z",
+  },
+  {
+    id: "puq72flptz",
+    author: {
+      id: "29r39bb8kdr",
+      name: "Julie",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453593-560d6ae3-0e11-44dd-90f5-f0b87a8b4ce9.jpg",
+      username: "julie",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451350-2c2d995e-9fce-45a0-a65a-9d5fa412702a.jpeg",
+    caption: "Turtle paradise",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-26T01:12:00.000Z",
+  },
+  {
+    id: "fdok8tb7x5",
+    author: {
+      id: "2gcwh1ccdfx",
+      name: "Kevin",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453596-ea862041-f1de-4e71-880b-2573c1f47ce8.jpg",
+      username: "kevin",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451331-12e2bf6c-d4d5-4df3-b445-75d986ae86a1.jpeg",
+    caption: "Summer breeze",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-25T19:59:00.000Z",
+  },
+  {
+    id: "1voze4jxpyp",
+    author: {
+      id: "2gcwh1ccdfx",
+      name: "Kevin",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453596-ea862041-f1de-4e71-880b-2573c1f47ce8.jpg",
+      username: "kevin",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451328-6c4b2bd5-3443-44b7-ac55-2b7dfa0176de.jpeg",
+    caption: "Farm life",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-25T14:41:00.000Z",
+  },
+  {
+    id: "1ucpa9b6tnh",
+    author: {
+      id: "vpk114e74l",
+      name: "Liza",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453597-cc7bff73-b838-4db6-a137-6706805195bf.jpg",
+      username: "liza",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451326-beed0b86-28f2-4979-8250-d304b57e7801.jpeg",
+    caption: "Enjoy life for it's beauty",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-25T12:32:00.000Z",
+  },
+  {
+    id: "4n6narr4iv",
+    author: {
+      id: "vpk114e74l",
+      name: "Liza",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453597-cc7bff73-b838-4db6-a137-6706805195bf.jpg",
+      username: "liza",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451325-5bbd11b9-2ed9-4166-9987-ac52abfeb637.jpeg",
+    caption: "Arctic Penguins",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-24T03:23:00.000Z",
+  },
+  {
+    id: "14jwvglyutl",
+    author: {
+      id: "2fd9ok8eaaz",
+      name: "Zach",
+      profilePhoto:
+        "https://user-images.githubusercontent.com/369384/192453599-affcb8f9-b475-40c1-94dc-2f7d23f820c6.jpg",
+      username: "zach",
+    },
+    photo:
+      "https://user-images.githubusercontent.com/369384/192451321-c511a886-1ecd-42dd-9afc-a1f2ae86d75b.jpeg",
+    caption: "Turnips are delicious",
+    likeCount: 0,
+    commentCount: 0,
+    isLikedByViewer: false,
+    createdAt: "2022-09-24T00:14:00.000Z",
+  },
+];
