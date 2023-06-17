1c44d0a2ac25b50188b65c8686539ab005ee1170
Simon <sstur@me.com>
2023-06-17 11:58:51 -0700

    Task 2 solution

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "expo-router";
-import { Image, Paragraph, ScrollView, YStack } from "tamagui";
+import { Image, Paragraph, ScrollView, XStack, YStack } from "tamagui";
 import { useQuery } from "@tanstack/react-query";
 import { getPosts } from "../src/api/getPosts";
 
@@ -15,10 +15,19 @@ export default () => {
   }
 
   return (
-    <ScrollView flex={1}>
+    <ScrollView flex={1} backgroundColor="white">
       <Stack.Screen options={{ title: "Home" }} />
       {result.data?.map((post) => (
         <YStack key={post.id}>
+          <XStack padding={8} alignItems="center" space={10}>
+            <Image
+              width={40}
+              height={40}
+              borderRadius={20}
+              source={{ uri: post.author.profilePhoto }}
+            />
+            <Paragraph>{post.author.name}</Paragraph>
+          </XStack>
           <Image source={{ uri: post.photo }} aspectRatio={1} />
           <Paragraph padding={10}>{post.caption}</Paragraph>
         </YStack>
