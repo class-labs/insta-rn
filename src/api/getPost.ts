895ad46577e0a1f7c46cb31e653ac8ae16eed4fc
Simon <sstur@me.com>
2023-06-16 22:35:57 -0700

    Update type FullPost

--- src/api/getPost.ts
+++ src/api/getPost.ts
@@ -1,8 +1,8 @@
-import { Post } from "../types/Post";
+import { FullPost } from "../types/FullPost";
 import { getAuthHeaders } from "./getAuthHeaders";
 import { toUrl } from "./toUrl";
 
-export async function getPost(id: string): Promise<Post> {
+export async function getPost(id: string): Promise<FullPost> {
   const response = await fetch(toUrl(`/posts/${id}`), {
     headers: getAuthHeaders(),
   });
