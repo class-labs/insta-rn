8586496a4035ad719ab7dd55fabf5095f6420175
Simon <sstur@me.com>
2023-06-16 22:04:47 -0700

    Implement post details page

--- src/components/FeedPostItem.tsx
+++ src/components/FeedPostItem.tsx
@@ -9,6 +9,7 @@ import { FeedPost } from "../types/FeedPost";
 import { formatRelativeTime } from "../support/formatRelativeTime";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { sendLikePost } from "../api/sendLikePost";
+import { useRouter } from "expo-router";
 
 type Props = {
   post: FeedPost;
@@ -16,6 +17,7 @@ type Props = {
 
 export function FeedPostItem(props: Props) {
   const { post } = props;
+  const router = useRouter();
   const queryClient = useQueryClient();
   const { mutate: likePost } = useMutation(sendLikePost, {
     onSuccess: (post) => {
@@ -34,47 +36,56 @@ export function FeedPostItem(props: Props) {
     createdAt,
   } = post;
   return (
-    <YStack>
-      <XStack px={16} py={12} alignItems="center" space={8}>
-        <Avatar circular size="$4">
-          <Avatar.Image source={{ uri: author.profilePhoto }} />
-          <Avatar.Fallback>
-            <YStack flex={1} jc="center" ai="center">
-              <Paragraph>{getInitials(author.name)}</Paragraph>
-            </YStack>
-          </Avatar.Fallback>
-        </Avatar>
-        <Paragraph>{author.name}</Paragraph>
-      </XStack>
-      <Image source={{ uri: post.photo }} aspectRatio={1} resizeMode="cover" />
-      <XStack px={16} py={12} space={12}>
-        <Pressable
-          style={({ pressed }) => (pressed ? { opacity: 0.5 } : undefined)}
-          onPress={() => likePost(post.id)}
-        >
-          <IconHeart color={isLikedByViewer ? "#f7444e" : "black"} />
-        </Pressable>
-        <Pressable
-          style={({ pressed }) => (pressed ? { opacity: 0.5 } : undefined)}
-          onPress={() => {
-            // TODO
-          }}
-        >
-          <IconMessageSquare />
-        </Pressable>
-      </XStack>
-      <YStack px={16} pb={12} space={8}>
-        <Paragraph fontWeight="600">
-          {likeCount === 1 ? "1 like" : `${likeCount.toLocaleString()} likes`}
-        </Paragraph>
-        <Paragraph opacity={0.8}>{caption}</Paragraph>
-        <Paragraph fontWeight="600">
-          {commentCount === 1
-            ? "View 1 comment"
-            : `View ${commentCount.toLocaleString()} comments`}
-        </Paragraph>
-        <Paragraph opacity={0.4}>{formatRelativeTime(createdAt)}</Paragraph>
+    <Pressable onPress={() => router.push(`/posts/${post.id}`)}>
+      <YStack>
+        <XStack px={16} py={12} alignItems="center" space={8}>
+          <Avatar circular size="$4">
+            <Avatar.Image source={{ uri: author.profilePhoto }} />
+            <Avatar.Fallback>
+              <YStack flex={1} jc="center" ai="center">
+                <Paragraph>{getInitials(author.name)}</Paragraph>
+              </YStack>
+            </Avatar.Fallback>
+          </Avatar>
+          <Paragraph>{author.name}</Paragraph>
+        </XStack>
+        <Image
+          source={{ uri: post.photo }}
+          aspectRatio={1}
+          resizeMode="cover"
+        />
+        <XStack px={16} py={12} space={12}>
+          <Pressable
+            style={({ pressed }) => (pressed ? { opacity: 0.5 } : undefined)}
+            onPress={() => likePost(post.id)}
+          >
+            <IconHeart color={isLikedByViewer ? "#f7444e" : "black"} />
+          </Pressable>
+          <Pressable
+            style={({ pressed }) => (pressed ? { opacity: 0.5 } : undefined)}
+            onPress={() => {
+              router.push({
+                pathname: `/posts/${post.id}`,
+                params: { intent: "comment" },
+              });
+            }}
+          >
+            <IconMessageSquare />
+          </Pressable>
+        </XStack>
+        <YStack px={16} pb={12} space={8}>
+          <Paragraph fontWeight="600">
+            {likeCount === 1 ? "1 like" : `${likeCount.toLocaleString()} likes`}
+          </Paragraph>
+          <Paragraph opacity={0.8}>{caption}</Paragraph>
+          <Paragraph fontWeight="600">
+            {commentCount === 1
+              ? "View 1 comment"
+              : `View ${commentCount.toLocaleString()} comments`}
+          </Paragraph>
+          <Paragraph opacity={0.4}>{formatRelativeTime(createdAt)}</Paragraph>
+        </YStack>
       </YStack>
-    </YStack>
+    </Pressable>
   );
 }
