874e076efab589bebc11aa169a0f3c92b09c3af9
Simon <sstur@me.com>
2024-08-21 09:31:07 -0700

    Setup for task 1

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,21 @@
-import { Paragraph } from "tamagui";
+import { Button, Paragraph, View } from "tamagui";
+
+async function getPosts() {
+  // TODO: Use fetch() to get the posts
+  // The URL is: https://insta-api.web-api.dev/posts
+}
 
 export function HomeScreen() {
-  return <Paragraph>Feed will go here...</Paragraph>;
+  return (
+    <View padding={20} gap={20}>
+      <Button
+        onPress={() => {
+          // TODO: Fetch posts.
+        }}
+      >
+        Click to get posts
+      </Button>
+      <Paragraph>A list of "post.caption" should go here</Paragraph>
+    </View>
+  );
 }
