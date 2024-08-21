1756b995e86e40ecd37bd4fc5da356ed136182e8
Simon <sstur@me.com>
2024-08-21 09:28:20 -0700

    Fetch posts on button press

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,29 @@
-import { Paragraph } from "tamagui";
+import { useState } from "react";
+import { Button, Paragraph, View } from "tamagui";
+
+import { Post } from "../types/Post";
+
+async function getPosts() {
+  const response = await fetch("https://insta-api.web-api.dev/posts");
+  const jsonData = await response.json();
+  return jsonData as Array<Post>;
+}
 
 export function HomeScreen() {
-  return <Paragraph>Feed will go here...</Paragraph>;
+  const [posts, setPosts] = useState<Array<Post>>([]);
+  return (
+    <View padding={20} gap={20}>
+      <Button
+        onPress={async () => {
+          const posts = await getPosts();
+          setPosts(posts);
+        }}
+      >
+        Click to get posts
+      </Button>
+      {posts.map((post) => {
+        return <Paragraph key={post.id}>{post.caption}</Paragraph>;
+      })}
+    </View>
+  );
 }
