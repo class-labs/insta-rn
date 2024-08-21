1163623f489dbd0f154876c0b1125b0979ea2d74
Simon <sstur@me.com>
2024-08-21 06:14:17 -0700

    Add @testing-library/react-native

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,75 +1,9 @@
-import { useEffect } from "react";
-import { Alert, Pressable, RefreshControl } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { LogOut as IconLogOut, Plus as IconPlus } from "@tamagui/lucide-icons";
-import { useQuery } from "@tanstack/react-query";
-import { Paragraph, ScrollView } from "tamagui";
-
-import { getPosts } from "../api/getPosts";
-import { FeedPostItem } from "../components/FeedPostItem";
-import { useAuth } from "../support/Auth";
-
-function HeaderLeft() {
-  const { isLoggedIn, setAuthToken } = useAuth();
-  return isLoggedIn ? (
-    <Pressable
-      style={({ pressed }) => (pressed ? { opacity: 0.5 } : undefined)}
-      onPress={() => {
-        setAuthToken(null);
-        Alert.alert("Logged Out", "You have been logged out.");
-      }}
-    >
-      <IconLogOut />
-    </Pressable>
-  ) : null;
-}
-
-function HeaderRight() {
-  const navigation = useNavigation();
-  const { isLoggedIn } = useAuth();
-  return (
-    <Pressable
-      style={({ pressed }) => (pressed ? { opacity: 0.5 } : undefined)}
-      onPress={() => {
-        if (isLoggedIn) {
-          navigation.navigate("NewPost");
-        } else {
-          navigation.navigate("Login");
-        }
-      }}
-    >
-      <IconPlus />
-    </Pressable>
-  );
-}
+import { Paragraph, YStack } from "tamagui";
 
 export function HomeScreen() {
-  const navigation = useNavigation();
-  const { data, error, isLoading, refetch } = useQuery(["posts"], getPosts);
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerLeft: () => <HeaderLeft />,
-      headerRight: () => <HeaderRight />,
-    });
-  }, [navigation]);
-
-  if (error) {
-    return <Paragraph>{String(error)}</Paragraph>;
-  }
-  if (!data) {
-    return <Paragraph>Loading...</Paragraph>;
-  }
   return (
-    <ScrollView
-      flex={1}
-      refreshControl={
-        <RefreshControl refreshing={isLoading} onRefresh={refetch} />
-      }
-    >
-      {data.map((post) => (
-        <FeedPostItem key={post.id} post={post} />
-      ))}
-    </ScrollView>
+    <YStack flex={1} justifyContent="center" alignItems="center">
+      <Paragraph>Hello world!</Paragraph>
+    </YStack>
   );
 }
