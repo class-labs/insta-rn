ce83ab94ce71e39c087120fbaeb0dc93796df6b1
Simon <sstur@me.com>
2024-08-21 12:43:54 -0700

    Task 4 solution

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,3 +1,5 @@
+import { Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { useQuery } from "@tanstack/react-query";
 import { Image, Paragraph, ScrollView, Spinner, View } from "tamagui";
 
@@ -5,6 +7,7 @@ import { getPosts } from "../api/getPosts";
 
 export function HomeScreen() {
   const { isLoading, error, data } = useQuery(["getPosts"], getPosts);
+  const navigation = useNavigation();
 
   if (error) {
     return (
@@ -27,10 +30,16 @@ export function HomeScreen() {
       <View gap={20}>
         {data.map((post) => {
           return (
-            <View key={post.id}>
-              <Image aspectRatio={1} source={{ uri: post.photo }} />
-              <Paragraph paddingHorizontal={20}>{post.caption}</Paragraph>
-            </View>
+            <Pressable
+              onPress={() => {
+                navigation.navigate("PostDetails");
+              }}
+            >
+              <View key={post.id}>
+                <Image aspectRatio={1} source={{ uri: post.photo }} />
+                <Paragraph paddingHorizontal={20}>{post.caption}</Paragraph>
+              </View>
+            </Pressable>
           );
         })}
       </View>
